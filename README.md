0ba12f7b69d882a70eb088b7385f7f08bdc39169
Teduh Adhi <teduhadhi@gmail.com>
2024-07-25 15:17:41 +0700

    add animation and update readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/ud456hEF)
-# assignment-module-3
+<!-- [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/ud456hEF)
+# assignment-module-3 -->
+
+# <h1 align ="center"> [Random Recipe Finder 🍕](https://www.bekerja.site/) </h1>
+
+**[Random Recipe Finder](https://www.bekerja.site/)** is here to help people who want to learn how to cook but do not know where to start, how to do it, or even decide what to cook. With millions of recipes on **[Random Recipe Finder](https://www.bekerja.site/)**, you will not get bored with the same meal, or even better, you will be a better chef in no time just by following along the step by step 👍.
+
+**[Link to Random Recipe Finder](https://www.bekerja.site/)**
+
+## Preview
+
+Just by a click of a button! 
+
+
+<img src="./src\Readme\main-page.png" alt="preview" width="400px"/>
+
+
+<img src="./src\Readme\recipe-example.png" alt="preview" width="400px"/>
--- src/main.js
+++ src/main.js
@@ -5,23 +5,40 @@ const recipe = {
   image: "https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg",
 };
 
-let separateInstructions =recipe.instructions.split(/\s\d/);
+
+//split the instructions into multiple strings, then store them into an array.
+let separateInstructions =recipe.instructions.split(/\d\.\s/);
 let instructionOrders = separateInstructions.length;
 
-for (let i = 1 ; i < instructionOrders ; i++){
-  let numbering = i + 1;
-  separateInstructions[i] = "\n" + numbering + separateInstructions[i]
-}
+// declare a variable to prevent a repetitive list being added.
+let addedList = 0;
 
-let newInstructions = separateInstructions.join("");
-
-console.log(newInstructions)
-
-
+//add event listener to call an empty function when the selected button is being clicked.
 document.querySelector("#getRecipeBtn").addEventListener("click",function(){
-// alert('button activated');
-document.querySelector(".recipe-container").setAttribute("style", "display:flex");
-document.querySelector(".recipe-title").innerText = recipe.title;
-document.querySelector(".recipe-image").setAttribute("src", recipe.image);
-document.querySelector(".recipe-instructions").innerText = newInstructions;
+
+  //activate the strecth animation by adding the animation attribute.
+  document.querySelector(".container").classList.add("animation-stretch");
+
+  //change the display attribute from none to flex.
+  document.querySelector(".recipe-container").setAttribute("style", "display:flex");
+
+  //add properties to each of the selected class.
+  document.querySelector(".recipe-title").innerText = recipe.title;
+  document.querySelector(".recipe-image").setAttribute("src", recipe.image);
+
+  //if the list items has not been added yet, add the list items from the array that had been made previously.
+  if(addedList<1){
+    for(let i = 1 ; i < instructionOrders; i++){
+      const node = document.createElement("li");
+      const textnode = document.createTextNode(separateInstructions[i]);
+      node.appendChild(textnode);
+      document.querySelector(".recipe-instructions").appendChild(node)
+    }
+  }
+
+  //all of the list items has been exported to the html file
+  addedList++;
+  return addedList;
+
+
 });
