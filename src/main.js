366d9a8fe3e8eb4f3125cd1cca1db8ac6c975078
Teduh Adhi <teduhadhi@gmail.com>
2024-08-02 19:16:55 +0700

    fetch API update

--- src/main.js
+++ src/main.js
@@ -1,47 +1,70 @@
-const recipe = {
-  title: "Spaghetti Carbonara",
-  instructions:
-    "1. Cook spaghetti according to package instructions. 2. In a separate pan, cook pancetta until crispy. 3. In a bowl, whisk eggs and grated cheese. 4. Combine spaghetti, pancetta, and egg mixture, stirring quickly. 5. Serve immediately with additional cheese and black pepper.",
-  image: "https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg",
-};
-
-
-let separateInstructions  = recipe.instructions.split(/\d\.\s/);
-let instructionOrders     = separateInstructions.length;
-
-console.log(separateInstructions);
-
-// let addedList             = 0;
 
 let container             = document.querySelector(".container");
 let recipeTitle           = document.querySelector(".recipe-title");
 let recipeButton          = document.querySelector("#getRecipeBtn");
+let recipeButton2         = document.querySelector("#getRecipeBtn2");
 let recipeContainer       = document.querySelector(".recipe-container");
 let recipeImage           = document.querySelector(".recipe-image");
 let recipeInstructions    = document.querySelector(".recipe-instructions");
+let listAdded             = false;
 
 recipeButton.onclick = onRandomRecipe;
+recipeButton2.onclick = onRandomRecipe;
 
-function onRandomRecipe(){
+async function onRandomRecipe() {
+
+  await getData();
 
   container.classList.add("animation-stretch");
   recipeContainer.classList.add("animation-fade-in");
   recipeContainer.style     = "display:flex";
   recipeButton.style        = "display:none";
-  recipeTitle.innerText     = recipe.title;
-  recipeImage.src           = recipe.image;
-  recipeImage.style.width   = "200px";
+  };
 
+const getData = async () =>{
+  try {
+    const respone = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+    const fetchedData = await respone.json();
+    const recipeArray = fetchedData.meals[0];
+    // console.log(recipeArray.strInstructions);
 
-  // if(addedList < 1){
-    for(let i = 1; i < instructionOrders; i++){
+    makeInstructionList(recipeArray.strInstructions);
+    embedData(recipeArray);
+    // throw new Error(error);
+
+  } catch (error) {
+    console.log(error.message);
+    recipeTitle.innerText     = error.message;
+    listRemover();
+  }
+}
+
+const makeInstructionList = (recipe) => {
+  let separateInstructions  = recipe.split(/\r\n/);
+  let instructionOrders     = separateInstructions.length;
+  listRemover();
+
+    for(let i = 0; i < instructionOrders; i++){
       const node      = document.createElement("li");
       const textnode  = document.createTextNode(separateInstructions[i]);
       node.appendChild(textnode);
-      recipeInstructions.appendChild(node)
-    }
+      recipeInstructions.appendChild(node);
+      listAdded = true;
+    } 
+}
 
-  // };
-  // addedList++;
-  // return addedList;
-  };
+const embedData = (recipe) => {
+  recipeTitle.innerText     = recipe.strMeal;
+  recipeImage.src           = recipe.strMealThumb;
+}
+
+
+const listRemover = () => {
+  if(listAdded){
+    let listArray = document.querySelectorAll('li');
+    for (let i = 0; i < listArray.length; i++) {
+      let list = document.querySelector('li');
+      list.remove()
+    }
+    }
+}
